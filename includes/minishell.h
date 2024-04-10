e99bcc00fa8a96d22eb629acb641375393bb6073
Goh Yong Siang Ivan <gyong-si@c1r2s4.42singapore.sg>
2024-04-10 13:47:04 +0800

    added makefile and libft files

---
+++ includes/minishell.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gyong-si <gyongsi@student.42.fr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/10 13:39:49 by gyong-si          #+#    #+#             */
+/*   Updated: 2024/04/10 13:42:08 by gyong-si         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include "../libraries/libft/libft.h"
+# include <unistd.h>
+# include <sys/wait.h>
+# include <fcntl.h>
+# include <errno.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+
+#endif

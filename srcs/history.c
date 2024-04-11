4c739b69030b650e877e8e0f0e9f955156a232eb
gyong-si <gyong-si@gyong-si.gyong-si.virtualbox.org>
2024-04-11 14:13:16 +0800

    history and clear history working

--- srcs/history.c
+++ srcs/history.c
@@ -1,27 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   minishell.h                                        :+:      :+:    :+:   */
+/*   history.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gyong-si <gyong-si@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/04/10 13:39:49 by gyong-si          #+#    #+#             */
-/*   Updated: 2024/04/11 12:02:27 by gyong-si         ###   ########.fr       */
+/*   Created: 2024/04/11 11:32:46 by gyong-si          #+#    #+#             */
+/*   Updated: 2024/04/11 11:59:08 by gyong-si         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef MINISHELL_H
-# define MINISHELL_H
+#include "../includes/minishell.h"
 
-# include "../libft/includes/libft.h"
-# include <unistd.h>
-# include <sys/wait.h>
-# include <fcntl.h>
-# include <errno.h>
-# include <stdio.h>
-# include <stdlib.h>
-# include <string.h>
-# include <readline/readline.h>
-# include <readline/history.h>
-
-#endif
+/**
+1. keep track of how many commands user have entered in history
+2. save the commands user have entered into a history.txt file
+3. when user enters history, print out all the user history
+4. when user enters history -c, clears out all the history file
+*/
--- srcs/main.c
+++ srcs/main.c
@@ -6,31 +6,79 @@
 /*   By: gyong-si <gyong-si@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/10 13:37:14 by gyong-si          #+#    #+#             */
-/*   Updated: 2024/04/11 10:18:20 by gyong-si         ###   ########.fr       */
+/*   Updated: 2024/04/11 14:12:05 by gyong-si         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
+int ft_strcmp(const char *s1, const char *s2)
+{
+	int	i;
+
+	i = 0;
+	while (s1[i] && s2[i])
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+		i++;
+	}
+	if (!s1[i] && !s2[i])
+		return (0);
+	return (1);
+}
+
 void	prompt()
 {
 	printf("minishell$");
 	fflush(stdout);
-	usleep(500000);
-	fflush(stdout);
-	usleep(500000);
+	usleep(5000);
 }
 
+void	print_history()
+{
+	//HISTORY_STATE *myhist = history_get_history_state();
+	HIST_ENTRY	**mylist = history_list();
+	int	i;
+
+	i = 0;
+	if (!mylist)
+	{
+		printf("Error getting history list\n");
+		return ;
+	}
+	while (1)
+	{
+		if (mylist[i] != NULL)
+			printf("%d: %s\n", i + 1, mylist[i]->line);
+		else
+			break ;
+		i++;
+	}
+}
 int	main()
 {
 	char	*input;
 
+	using_history();
 	while (1)
 	{
 		prompt();
 		input = readline(" ");
-		if (input == NULL)
-			break ;
+		if (input != NULL || *input != '\0')
+		{
+			printf("This is user input: %s\n", input);
+			add_history(input);
+			if (ft_strcmp(input, "history") == 0)
+			{
+				print_history();
+			}
+			if (ft_strcmp(input, "history -c") == 0)
+			{
+				clear_history();
+			}
+		}
+		free(input);
 	}
 	return (0);
 }

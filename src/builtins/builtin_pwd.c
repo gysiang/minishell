e794c44ff8190ab2dcfe834d610cb9424cf40254
Axel Lee <axellee1994@gmail.com>
2024-04-12 12:29:42 +0800

    include limits.h and builtin_env

--- src/builtins/builtin_pwd.c
+++ src/builtins/builtin_pwd.c
@@ -1,29 +1,30 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   builtin_env.c                                      :+:      :+:    :+:   */
+/*   builtin_pwd.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: axlee <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/04/12 11:06:17 by axlee             #+#    #+#             */
-/*   Updated: 2024/04/12 12:26:46 by axlee            ###   ########.fr       */
+/*   Created: 2024/04/12 12:13:16 by axlee             #+#    #+#             */
+/*   Updated: 2024/04/12 12:25:43 by axlee            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	minishell_env(t_shell *minishell, t_cmd *cmd)
+int	minishell_pwd(t_shell *minishell, t_cmd *cmd)
 {
-	int	i;
+	char	pwd[PATH_MAX];
 
-	(void)cmd;
-	i = 0;
-	if (!minishell->env[i])
-		break ;
-	while (minishell->env[i])
+	if (getcwd(pwd, sizeof(pwd)) == NULL)
 	{
-		ft_putendl(minishell->env[i]);
-		i++;
+		perror("Error retrieving current directory");
+		cmd->return_value = 1;
 	}
-	return (0);
+	else
+	{
+		printf("%s\n", pwd);
+		cmd->return_value = 0;
+	}
+	return (cmd->return_value);
 }

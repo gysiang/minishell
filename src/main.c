cb182eb84c69ae66ea1e66963cffcd5062394220
Goh Yong Siang Ivan <gyong-si@c2r2s7.42singapore.sg>
2024-04-28 16:42:53 +0800

    fixed path issue with get_path

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: gyong-si <gyongsi@student.42.fr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/10 13:37:14 by gyong-si          #+#    #+#             */
-/*   Updated: 2024/04/28 14:32:32 by gyong-si         ###   ########.fr       */
+/*   Updated: 2024/04/28 16:39:21 by gyong-si         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,18 @@ for (int i = 0; s[i] != NULL; i++) {
 	printf("av[%d]: %s\n", i, s[i]);
 }; **/
 
-int	main(int ac, char **av, char **env)
+int	main(int ac, char **av, char **envp)
 {
 	(void)ac;
 	(void)av;
 	char		*line;
 	t_token		*token_lst;
-	t_ast_node	*ast_tree;
-
+	//t_ast_node	*ast_tree;
+	/**
+	for (int i = 0; envp[i] != NULL; i++)
+    {
+        printf("%s\n", envp[i]);
+    } **/
 	using_history();
 	setup_signal_handler();
 	while (1)
@@ -40,15 +44,15 @@ int	main(int ac, char **av, char **env)
 		add_history(line);
 		//printf("This is user input: %s\n", line);
 		token_lst = token_processor(line);
-		//print_tokenlst(token_lst);
 		// make and print ast_tree
-		ast_tree = make_ast_tree(token_lst);
-		print_ast_tree(ast_tree);
+		//ast_tree = make_ast_tree(token_lst);
+		//print_tokenlst(token_lst);
+		//print_ast_tree(ast_tree);
 		if (hist_feature(line) == 1)
 			return (1);
-		pipex(token_lst, env);
+		pipex(token_lst, envp);
 		free_tokenlst(token_lst);
-		free_ast_tree(ast_tree);
+		//free_ast_tree(ast_tree);
 	}
 	return (0);
 }

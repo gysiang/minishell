fb09341b17fe8bc0d5eb8f4990ea474c4365da0e
Axel Lee <axellee1994@gmail.com>
2024-05-17 14:31:17 +0800

    Norminette env_manager.c

--- src/env_manager.c
+++ src/env_manager.c
@@ -3,113 +3,82 @@
 /*                                                        :::      ::::::::   */
 /*   env_manager.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gyong-si <gyong-si@student.42.fr>          +#+  +:+       +#+        */
+/*   By: axlee <axlee@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/20 19:36:29 by gyong-si          #+#    #+#             */
-/*   Updated: 2024/05/06 09:59:28 by gyong-si         ###   ########.fr       */
+/*   Updated: 2024/05/17 14:26:53 by axlee            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-char *get_env_value(t_shell *minishell, const char *var)
+void	sort_env(t_shell *minishell)
 {
-    int i;
-    size_t  var_len;
+	int	i;
+	int	j;
+	int	environment_len;
 
-    i = 0;
-    var_len = ft_strlen(var);
-    while (minishell->env[i])
-    {
-        if (ft_strncmp(minishell->env[i], var, var_len) == 0 && minishell->env[i][var_len] == '=')
-            return ft_strdup(&minishell->env[i][var_len + 1]);
-        i++;
-    }
-    return (NULL);
+	i = 0;
+	environment_len = env_len(minishell);
+	while (i < environment_len - 1)
+	{
+		j = 0;
+		while (j < environment_len - i - 1)
+		{
+			if (ft_strccmp(minishell->env[j], minishell->env[j + 1], '=') > 0)
+				ft_strswap(&minishell->env[j], &minishell->env[j + 1]);
+			j++;
+		}
+		i++;
+	}
 }
 
-int env_len(t_shell *minishell)
+static void	set_env_entry(t_shell *minishell, char *new_entry, int idx)
 {
-    int i;
+	int	current_length;
 
-    i = 0;
-    while (minishell->env[i])
-        i++;
-    return (i);
+	if (idx != -1)
+	{
+		free(minishell->env[idx]);
+		minishell->env[idx] = new_entry;
+	}
+	else
+	{
+		current_length = env_len(minishell);
+		if (minishell->env_size <= current_length + 1)
+			env_realloc(minishell);
+		minishell->env[current_length] = new_entry;
+		minishell->env[current_length + 1] = NULL;
+	}
 }
 
-int search_env_by_var(t_shell *minishell, const char *var)
+void	set_env(t_shell *minishell, const char *var, const char *value)
 {
-    int i = 0;
-    size_t var_len;
+	int		idx;
+	size_t	var_len;
+	size_t	value_len;
+	char	*new_entry;
 
-    var_len = ft_strlen(var);
-    i = 0;
-    while (minishell->env[i])
-    {
-        if (ft_strncmp(minishell->env[i], var, var_len) == 0 && minishell->env[i][var_len] == '=')
-            return i;
-        i++;
-    }
-    return (-1);
+	idx = search_env_by_var(minishell, var);
+	var_len = ft_strlen(var);
+	value_len = ft_strlen(value);
+	new_entry = malloc(var_len + value_len + 2);
+	if (!new_entry)
+	{
+		perror("Failed to allocate memory for new environment variable");
+		return ;
+	}
+	ft_strcpy(new_entry, var);
+	new_entry[var_len] = '=';
+	ft_strcpy(&new_entry[var_len + 1], value);
+	set_env_entry(minishell, new_entry, idx);
 }
 
-void  sort_env(t_shell *minishell)
+void	init_env(t_shell *minishell, char **envp)
 {
-    int i;
-    int j;
-    int environment_len;
-
-    i = 0;
-    environment_len = env_len(minishell);
-    while (i < environment_len - 1)
-    {
-        j = 0;
-        while (j < environment_len - i - 1)
-        {
-            if (ft_strccmp(minishell->env[j], minishell->env[j + 1], '=') > 0)
-                ft_strswap(&minishell->env[j], &minishell->env[j + 1]);
-            j++;
-        }
-        i++;
-    }
-}
-
-//Need to split this into helper functions
-void set_env(t_shell *minishell, const char *var, const char *value)
-{
-    int idx = search_env_by_var(minishell, var);
-    size_t var_len = ft_strlen(var);
-    size_t value_len = ft_strlen(value);
-    char *new_entry = malloc(var_len + value_len + 2);
-    if (!new_entry)
-    {
-        perror("Failed to allocate memory for new environment variable");
-        return;
-    }
-    ft_strcpy(new_entry, var);
-    new_entry[var_len] = '=';
-    ft_strcpy(&new_entry[var_len + 1], value);
-    if (idx != -1)
-    {
-        free(minishell->env[idx]);
-        minishell->env[idx] = new_entry;
-    }
-    else
-    {
-        int current_length = env_len(minishell);
-        if (minishell->env_size <= current_length + 1)
-            env_realloc(minishell);
-        minishell->env[current_length] = new_entry;
-        minishell->env[current_length + 1] = NULL;
-    }
-}
-
-void init_env(t_shell *minishell, char **envp)
-{
-	int i;
-	int j;
-	char *equal_sign;
+	int		i;
+	int		j;
+	char	*equal_sign;
 
 	i = 0;
 	j = 0;
@@ -118,7 +87,8 @@ void init_env(t_shell *minishell, char **envp)
 	while (envp[i])
 	{
 		equal_sign = ft_strchr(envp[i], '=');
-		if (equal_sign && !ft_strnstr(envp[i], "_WORKSPACE_", equal_sign - &envp[i][0]))
+		if (equal_sign && !ft_strnstr(envp[i], "_WORKSPACE_", equal_sign
+				- &envp[i][0]))
 		{
 			if (j == minishell->env_size - 1)
 				env_realloc(minishell);
---
+++ src/env_manager_utils.c
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   env_manager_utils.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: axlee <axlee@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/17 14:09:33 by axlee             #+#    #+#             */
+/*   Updated: 2024/05/17 14:29:45 by axlee            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+char	*get_env_value(t_shell *minishell, const char *var)
+{
+	int		i;
+	size_t	var_len;
+
+	i = 0;
+	var_len = ft_strlen(var);
+	while (minishell->env[i])
+	{
+		if (ft_strncmp(minishell->env[i], var, var_len) == 0
+			&& minishell->env[i][var_len] == '=')
+			return (ft_strdup(&minishell->env[i][var_len + 1]));
+		i++;
+	}
+	return (NULL);
+}
+
+int	env_len(t_shell *minishell)
+{
+	int	i;
+
+	i = 0;
+	while (minishell->env[i])
+		i++;
+	return (i);
+}
+
+int	search_env_by_var(t_shell *minishell, const char *var)
+{
+	int		i;
+	size_t	var_len;
+
+	var_len = ft_strlen(var);
+	i = 0;
+	while (minishell->env[i])
+	{
+		if (ft_strncmp(minishell->env[i], var, var_len) == 0
+			&& minishell->env[i][var_len] == '=')
+			return (i);
+		i++;
+	}
+	return (-1);
+}

eb5ba89f0b6a4fc8eb67fd86d60affa5eae06393
Axel Lee <axellee1994@gmail.com>
2024-05-17 11:33:09 +0800

    builtin_env.c norminette

--- src/builtins/builtin_env.c
+++ src/builtins/builtin_env.c
@@ -3,27 +3,27 @@
 /*                                                        :::      ::::::::   */
 /*   builtin_env.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gyong-si <gyong-si@student.42.fr>          +#+  +:+       +#+        */
+/*   By: axlee <axlee@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/12 11:06:17 by axlee             #+#    #+#             */
-/*   Updated: 2024/05/12 14:12:58 by gyong-si         ###   ########.fr       */
+/*   Updated: 2024/05/17 11:32:49 by axlee            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void minishell_env(t_shell *minishell)
+void	minishell_env(t_shell *minishell)
 {
-	int i;
+	int	i;
 
 	i = 0;
 	if (!minishell->env || !minishell->env[0])
-		return;
+		return ;
 	env_len(minishell);
 	while (minishell->env[i])
 	{
-		printf("env %d : ",i);
-		printf("%s\n",minishell->env[i]);
+		printf("env %d : ", i);
+		printf("%s\n", minishell->env[i]);
 		i++;
 	}
 }

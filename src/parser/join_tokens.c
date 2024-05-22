fa2cdec90ab6455f06bad099816cb7952c5ecf0c
Axel Lee <axellee1994@gmail.com>
2024-05-23 02:14:45 +0800

    Finally fixed minishell_cd.
    Properly split the tokens based on white space and now it is processing cd src as two tokens instead of one.

--- src/parser/join_tokens.c
+++ src/parser/join_tokens.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   join_tokens.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gyong-si <gyong-si@student.42.fr>          +#+  +:+       +#+        */
+/*   By: axlee <axlee@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/04 15:31:06 by gyong-si          #+#    #+#             */
-/*   Updated: 2024/05/06 09:47:56 by gyong-si         ###   ########.fr       */
+/*   Updated: 2024/05/23 02:11:48 by axlee            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,26 +47,40 @@ static void	merge_identifier_tokens(t_token *curr)
 	}
 }
 
+
+static bool is_command(char *token)
+{
+    // Implement a check to see if the token is a known command
+    static const char *commands[] = {"cd", "ls", "echo", "exit", NULL};
+    for (int i = 0; commands[i]; i++)
+    {
+        if (strcmp(token, commands[i]) == 0)
+            return true;
+    }
+    return false;
+}
+
 void	join_identifier_tokens(t_token *lst)
 {
-	t_token	*curr;
-	int		merged;
+    t_token	*curr;
+    int		merged;
 
-	merged = 1;
-	while (merged)
-	{
-		merged = 0;
-		curr = lst;
-		while (curr != NULL && curr->next != NULL)
-		{
-			if (curr->type == T_IDENTIFIER
-				&& curr->next->type == T_IDENTIFIER)
-			{
-				merge_identifier_tokens(curr);
-				merged = 1;
-				break ;
-			}
-			curr = curr->next;
-		}
-	}
+    merged = 1;
+    while (merged)
+    {
+        merged = 0;
+        curr = lst;
+        while (curr != NULL && curr->next != NULL)
+        {
+            // Check if both tokens are not just simple identifiers but potentially commands and arguments
+            if (curr->type == T_IDENTIFIER && curr->next->type == T_IDENTIFIER &&
+                !is_command(curr->token) && !is_command(curr->next->token))
+            {
+                merge_identifier_tokens(curr);
+                merged = 1;
+                break;
+            }
+            curr = curr->next;
+        }
+    }
 }
--- src/parser/parser.c
+++ src/parser/parser.c
@@ -6,7 +6,7 @@
 /*   By: axlee <axlee@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/17 13:35:24 by axlee             #+#    #+#             */
-/*   Updated: 2024/05/21 19:37:48 by axlee            ###   ########.fr       */
+/*   Updated: 2024/05/23 02:13:44 by axlee            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,7 @@ void	set_token_pointers(t_token *tokens)
 		prev_token->next = NULL;
 }
 
-void	handle_cd_command(t_token **curr, t_shell *minishell)
+/*void	handle_cd_command(t_token **curr, t_shell *minishell)
 {
 	t_token	*next_token;
 	char	*dir;
@@ -55,7 +55,7 @@ void	handle_cd_command(t_token **curr, t_shell *minishell)
 		minishell->last_return = 1;
 		*curr = (*curr)->next;
 	}
-}
+}*/
 
 void	parse_token(t_token *token, t_shell *minishell)
 {
@@ -70,8 +70,6 @@ void	parse_token(t_token *token, t_shell *minishell)
 		//parse_doublequote(token);
 		parse_value(token, minishell);
 	}
-	else if (strcmp(token->token, "cd") == 0)
-		handle_cd_command(&token, minishell);
 }
 
 t_token	*token_parser(t_token *token_lst, t_shell *minishell)

f5d517e05d0b04ed49007958b2de7be86c2b4473
axellee1994 <axellee1994@gmail.com>
2024-06-09 13:47:34 +0800

    Double quotes capturing last signal now. Need to think of another way to implement sprintf.

--- src/parser/parse_double_quotes_utils_2.c
+++ src/parser/parse_double_quotes_utils_2.c
@@ -6,13 +6,14 @@
 /*   By: axlee <axlee@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/09 12:04:18 by axlee             #+#    #+#             */
-/*   Updated: 2024/06/09 12:42:20 by axlee            ###   ########.fr       */
+/*   Updated: 2024/06/09 13:46:00 by axlee            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	initialize_parse_variables(t_token *token, char **str, int *len, char **result)
+void	initialize_parse_variables(t_token *token, char **str, int *len,
+		char **result)
 {
 	*str = token->token;
 	*len = ft_strlen(*str) * 2; // Allocate more space to handle expansions
@@ -23,26 +24,38 @@ void	initialize_parse_variables(t_token *token, char **str, int *len, char **res
 
 void	process_dollar_character(char *str, char *result, int *i, int *j)
 {
-	t_shell	*minishell;
-	char	next_char;
+    t_shell	*minishell;
+    char	next_char;
+    char    exit_status[12];
 
-	minishell = init_shell();
-	if (str[*i + 1] == '\0')
-	{
-		result[(*j)++] = '$';
-		(*i)++;
-	}
-	else
-	{
-		next_char = str[*i + 1];
-		if (!ft_isalnum(next_char) && next_char != '_' && next_char != '?')
-		{
-			result[(*j)++] = '$';
-			(*i)++;
-		}
-		else
-			handle_env_variable_expansion(str, result, i, minishell);
-	}
+    minishell = init_shell();
+    if (str[*i + 1] == '\0')
+    {
+        result[(*j)++] = '$';
+        (*i)++;
+    }
+    else
+    {
+        next_char = str[*i + 1];
+        if (next_char == '?')
+        {
+            int length;
+            length = sprintf(exit_status, "%d", minishell->last_return);
+            if (length > 0)
+            {
+                strcpy(&result[*j], exit_status);
+                *j += strlen(exit_status);
+            }
+            *i += 2; // Skip past the '$' and '?'
+        }
+        else if (!ft_isalnum(next_char) && next_char != '_')
+        {
+            result[(*j)++] = '$';
+            (*i)++;
+        }
+        else
+            handle_env_variable_expansion(str, result, i, minishell);
+    }
 }
 
 void	process_character(char *str, char *result, int *i, int *j)
@@ -56,5 +69,5 @@ void	process_character(char *str, char *result, int *i, int *j)
 		result[(*j)++] = str[*i];
 		(*i)++;
 	}
-	result[*j] = '\0'; // Ensure null-termination
+	result[*j] = '\0';
 }

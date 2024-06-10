ae977b96a6b94be75e58402fd485b2e163cb3340
gysiang <gysiang@gmail.com>
2024-06-10 15:40:38 +0800

    fix memory leak from cd 123

--- src/pipex/execute_utils.c
+++ src/pipex/execute_utils.c
@@ -6,7 +6,7 @@
 /*   By: gyong-si <gyong-si@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/06 10:21:39 by gyong-si          #+#    #+#             */
-/*   Updated: 2024/06/08 22:23:00 by gyong-si         ###   ########.fr       */
+/*   Updated: 2024/06/10 15:26:11 by gyong-si         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,7 +58,7 @@ char	*get_command_path(char **s_cmd, t_shell *minishell)
 		path = get_path(s_cmd[0], minishell);
 	if (!path)
 	{
-		printf("Command not found: %s\n", s_cmd[0]);
+		//printf("Command not found: %s\n", s_cmd[0]);
 		return_code = minishell_error_msg(s_cmd[0], 42);
 		minishell->last_return = return_code;
 		exit(1);

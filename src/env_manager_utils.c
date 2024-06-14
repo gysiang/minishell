9aaf79a750b970733734a45aeef0178ca857c75f
axellee1994 <axellee1994@gmail.com>
2024-06-14 15:34:24 +0800

    Resolved memory leaks when incorrect commands are issued

--- src/env_manager_utils.c
+++ src/env_manager_utils.c
@@ -6,17 +6,20 @@
 /*   By: axlee <axlee@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/17 14:09:33 by axlee             #+#    #+#             */
-/*   Updated: 2024/06/14 12:58:58 by axlee            ###   ########.fr       */
+/*   Updated: 2024/06/14 15:33:31 by axlee            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
 // If return (strdup == "", it will output echo $TEST as empty string but further issues at the back)
-char	*get_env_value(t_shell *minishell, const char *var)
+/*char	*get_env_value(t_shell *minishell, const char *var)
 {
 	int		i;
 	size_t	var_len;
+	int		i;
+	size_t	var_len;
+	char	*value;
 
 	var_len = ft_strlen(var);
 	i = 0;
@@ -31,6 +34,33 @@ char	*get_env_value(t_shell *minishell, const char *var)
 	}
 	// Return an empty string if the environment variable is not found
 	return (ft_strdup(""));
+}*/
+
+char	*get_env_value(t_shell *minishell, const char *var)
+{
+	int		i;
+	size_t	var_len;
+	char	*value;
+
+	value = NULL;
+	var_len = ft_strlen(var);
+	i = 0;
+	while (minishell->env[i])
+	{
+		if (ft_strncmp(minishell->env[i], var, var_len) == 0
+			&& minishell->env[i][var_len] == '=')
+		{
+			value = ft_strdup(&minishell->env[i][var_len + 1]);
+			if (value == NULL)
+			{
+				// Handle memory allocation failure
+				return (NULL);
+			}
+			return (value);
+		}
+		i++;
+	}
+	return (NULL);
 }
 
 // If return NULL, it will output echo $ as $ but will also output echo $TEST as $TEST
@@ -38,6 +68,7 @@ char	*get_env_value(t_shell *minishell, const char *var)
 {
 	int		i;
 	size_t	var_len;
+	int		i;
 
 	var_len = ft_strlen(var);
 	i = 0;
@@ -50,10 +81,9 @@ char	*get_env_value(t_shell *minishell, const char *var)
 	}
 	return (NULL);
 }*/
-
 int	env_len(t_shell *minishell)
 {
-	int	i;
+	int		i;
 
 	i = 0;
 	while (minishell->env[i])
--- src/sutils_2.c
+++ src/sutils_2.c
@@ -13,14 +13,59 @@
 
 #include "minishell.h"
 
-static size_t	ft_get_size(void *ptr)
+size_t	ft_get_size(char **array)
 {
+	size_t	size;
+
+	size = 0;
+	if (array == NULL)
+		return (0);
+	while (array[size] != NULL)
+		size++;
+	return (size);
+}
+
+/*static size_t	ft_get_size(void *ptr)
+{
+	void	*new_ptr;
+	size_t	old_size;
+	void	*new_ptr;
+	size_t	old_size;
+	void	*new_ptr;
+	size_t	old_size;
+	size_t	min_size;
+
 	if (ptr == NULL)
 		return (0);
 	return (*((size_t *)ptr - 1));
+}*/
+void	*ft_realloc(void *ptr, size_t new_size)
+{
+	size_t	old_size;
+	void	*new_ptr;
+	size_t	min_size;
+
+	if (new_size == 0)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	if (ptr == NULL)
+		return (malloc(new_size));
+	old_size = ft_get_size(ptr) * sizeof(char *);
+	new_ptr = malloc(new_size);
+	if (new_ptr == NULL)
+		return (NULL);
+	if (old_size < new_size)
+		min_size = old_size;
+	else
+		min_size = new_size;
+	ft_memcpy(new_ptr, ptr, min_size);
+	free(ptr);
+	return (new_ptr);
 }
 
-void	*ft_realloc(void *ptr, size_t new_size)
+/*void	*ft_realloc(void *ptr, size_t new_size)
 {
 	void	*new_ptr;
 	size_t	old_size;
@@ -42,8 +87,7 @@ void	*ft_realloc(void *ptr, size_t new_size)
 		ft_memcpy(new_ptr, ptr, new_size);
 	free(ptr);
 	return (new_ptr);
-}
-
+}*/
 void	reset_minishell(t_shell *minishell)
 {
 	minishell->prev_fd = -1;
@@ -52,4 +96,3 @@ void	reset_minishell(t_shell *minishell)
 	minishell->signal_received = 0;
 	minishell->flag = 0;
 }
-

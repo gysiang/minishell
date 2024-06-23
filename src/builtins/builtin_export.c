ea1a84eca0aef939c1466dbfc8b78ad2f925e891
axellee1994 <axellee1994@gmail.com>
2024-06-23 19:41:17 +0800

    Test 135: ✅ $EMPTY echo hi (All extras resolved)

--- src/builtins/builtin_export.c
+++ src/builtins/builtin_export.c
@@ -3,33 +3,19 @@
 /*                                                        :::      ::::::::   */
 /*   builtin_export.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gyong-si <gyong-si@student.42.fr>          +#+  +:+       +#+        */
+/*   By: axlee <axlee@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/19 15:05:01 by axlee             #+#    #+#             */
-/*   Updated: 2024/06/21 12:43:44 by gyong-si         ###   ########.fr       */
+/*   Updated: 2024/06/23 19:40:20 by axlee            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	minishell_export(t_shell *minishell)
+static int	process_export_tokens(t_token *current, t_shell *minishell)
 {
-	t_token	*tokens;
-	t_token	*current;
-	int		result;
+	int	result;
 
-	if (!minishell || !minishell->cmd_list)
-	{
-		printf("No command list provided.\n");
-		return (0);
-	}
-	tokens = minishell->cmd_list;
-	current = tokens->next;
-	if (!current)
-	{
-		print_vars(minishell);
-		return (0);
-	}
 	result = 0;
 	while (current)
 	{
@@ -41,4 +27,22 @@ int	minishell_export(t_shell *minishell)
 		current = current->next;
 	}
 	return (result);
+}
+
+int	minishell_export(t_shell *minishell)
+{
+	t_token	*current;
+
+	if (!minishell || !minishell->cmd_list)
+	{
+		printf("No command list provided.\n");
+		return (0);
+	}
+	current = minishell->cmd_list->next;
+	if (!current)
+	{
+		print_vars(minishell);
+		return (0);
+	}
+	return (process_export_tokens(current, minishell));
 }
